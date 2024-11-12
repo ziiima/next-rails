0a20a3b8b2456de4fdce07fcfab7c4fd109ab87c
ziiima <nagahama.takuya@gmail.com>
2024-11-12 09:03:37 +0900

    add composite Primary keys

--- api/app/models/book.rb
+++ api/app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
-  has_many :book_orders
+  has_many :book_orders, class_name: "Book::Order"
+  has_many :orders, through: :book_orders
 end
--- api/app/models/book/order.rb
+++ api/app/models/book/order.rb
@@ -1,4 +1,4 @@
 class Book::Order < ApplicationRecord
     belongs_to :book
-    belongs_to :order, foreign_key: [ :order_id, :order_shop_id ]
+    belongs_to :order, class_name: "::Order", foreign_key: [ :order_id, :order_shop_id ]
 end
